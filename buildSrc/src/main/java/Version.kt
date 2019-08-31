41a0ff83795b959c22c5c993eaf7736243ffdb3b
Saeed Masoumi <7masoumi@gmail.com>
2019-08-31 23:06:05 +0430

    Add project boilerplate

---
+++ buildSrc/src/main/java/Version.kt
@@ -0,0 +1,14 @@
+object Version {
+
+    val kotlin = "1.3.41"
+    val android = "3.4.2"
+    val minSdk = 16
+    val targetSdk = 29
+    val buildTools = "29.0.1"
+    val compileSdk = 29
+
+    val appcompat = "1.0.2"
+    val junit = "4.12"
+    val testRunner = "1.2.0"
+    val espresso = "3.2.0"
+}
